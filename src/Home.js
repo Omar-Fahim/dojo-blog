177db07bfc4a2207c2aa86eeb8e3c2cad6fd8c8b
Omar Shaaban <ofahim2002@gmail.com>
2023-09-24 20:55:54 +0300

    Adding a json server

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,10 @@ import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
+
+
+    
+
   const [blogs, setBlogs] = useState([
     { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
@@ -16,9 +20,9 @@ const Home = () => {
   }
 
   useEffect(() => {
-    console.log('use effect ran');
-  },[]) ;
-
+    console.log('use  ran');
+  },[name]) ;
+  
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
